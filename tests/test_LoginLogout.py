4d1fefc5ae554539fb264a8da9ce2e11dd0bf580
SadabSaqib <sadabnepal1993@gmail.com>
2021-02-08 22:24:20 +0530

    added screenshots for failed tests

--- tests/test_LoginLogout.py
+++ tests/test_LoginLogout.py
@@ -7,12 +7,15 @@ from pages.LoginPage import LoginPage
 
 class TestLoginLogout(BaseClass):
 
-    def test_e2e(self, login_data):
+    def test_LoginLogout(self, login_data):
         home_page = LoginPage(self.driver).setUsername(login_data["username"]).setPassword(login_data["password"]).clickLoginBtn()
         assert ("Welcome" in home_page.getLoggedInMsg().text)
 
         home_page.doLogout()
         assert LoginPage(self.driver).getLoginImageContainer().is_displayed() == True
+
+    def test_unit(self):
+        assert LoginPage(self.driver).getLoginImageContainer().is_displayed() == False
 
     @pytest.fixture(params=HomePageData.test_homepage_data)
     def login_data(self, request):
