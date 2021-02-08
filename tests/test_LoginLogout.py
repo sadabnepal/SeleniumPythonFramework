f2c9e66537d519de7b3f8d0710e170f439ad49ec
SadabSaqib <sadabnepal1993@gmail.com>
2021-02-09 00:13:40 +0530

    added excel util

--- tests/test_LoginLogout.py
+++ tests/test_LoginLogout.py
@@ -8,15 +8,22 @@ from pages.LoginPage import LoginPage
 class TestLoginLogout(BaseClass):
 
     def test_LoginLogout(self, login_data):
-        home_page = LoginPage(self.driver).setUsername(login_data["username"]).setPassword(login_data["password"]).clickLoginBtn()
+        home_page = LoginPage(self.driver).setUsername(login_data["username"]).setPassword(
+            login_data["password"]).clickLoginBtn()
         assert ("Welcome" in home_page.getLoggedInMsg().text)
 
         home_page.doLogout()
         assert LoginPage(self.driver).getLoginImageContainer().is_displayed() == True
 
-    def test_unit(self):
-        assert LoginPage(self.driver).getLoginImageContainer().is_displayed() == False
+    def test_InvalidLogin(self, invalid_login_data):
+        LoginPage(self.driver).setUsername(invalid_login_data["username"]).setPassword(
+            invalid_login_data["password"]).clickLoginBtn()
+        assert (LoginPage(self.driver).getInvalidMsgText() == "Invalid credentials")
 
     @pytest.fixture(params=HomePageData.test_homepage_data)
     def login_data(self, request):
+        return request.param
+
+    @pytest.fixture(params=HomePageData.invalid_login_data)
+    def invalid_login_data(self, request):
         return request.param
