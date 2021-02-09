69ea90d17de551319aa20cd3439eb96a07711001
SadabSaqib <sadabnepal1993@gmail.com>
2021-02-09 08:05:56 +0530

    fixed excel util

--- Utils/ExcelReader.py
+++ Utils/ExcelReader.py
@@ -12,3 +12,4 @@ class ExcelReader:
             if sheet.cell(row=i, column=1).value == test_name:
                 for j in range(2, sheet.max_row+1):
                     Dict[sheet.cell(row=1, column=j).value] = sheet.cell(row=i, column=j).value
+        return [Dict]
